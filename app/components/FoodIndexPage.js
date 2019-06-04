40b3b7735cfb2aaf4738633bc48f43e073a75650
Amirsalar Heydari <amirsalaar@gmail.com>
2019-06-04 13:52:43 -0700

    Add api requesto to FoodIndexScreen

--- app/components/FoodIndexPage.js
+++ app/components/FoodIndexPage.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
+import { Food } from '../services/food';
 
 export default class FoodIndexPage extends Component {
   state = {
-    foods:[],
+    foods: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.fetchFoods();
+  }
+
+  fetchFoods = () => {
+    Food.all().then(foods => {
+      this.setState({
+        foods, loading: false
+      })
+    })
+      .catch(err => this.setState({ loading: false }));
   };
 
   render() {
